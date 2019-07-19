0f17bafce0f4a108a0c16604bb5404031799f9a7
Patrick Lamoureux <patrick@blackcedar.com>
2019-07-19 11:14:27 -0600

    brain tree submit and drop

--- client/src/components/BraintreeDrop.js
+++ client/src/components/BraintreeDrop.js
@@ -1,16 +1,40 @@
-import React, { Component, } from 'react';
+import React, { useState, useEffect,  } from 'react';
 import { Dimmer, Loader, Segment, Header } from 'semantic-ui-react';
 import braintree from 'braintree-web-drop-in';
 import BraintreeDropin from 'braintree-dropin-react';
 import BraintreeSubmitButton from './BraintreeSubmitButton';
+import {Redirect} from 'react-router-dom'
+import axios from 'axios'
 
 const BraintreeDrop = (props) =>{
+  const [loaded, setLoaded] = useState(false)
+  const [token, setToken] = useState("")
+  const [redirect, setRedirect] = useState(false)
+  const [transactionID, setTransactionID] = useState('')
 
-  return(
-    <Segment>
-      <Header as = "h1" textAlign="center">Hello</Header>
-    </Segment>
+  const handlePaymentMethod = (payload) =>{
+    
+  }
+      if(loaded){
+        return(
+          <Segment>
+            <BraintreeDropin 
+              braintree={braintree}
+              authorization={token}
+              handlePaymentMethod={handlePaymentMethod}
+              renderSubmitButton={BraintreeSubmitButton}
+            />
+          </Segment>
+        )
+      }
+      else
+      return(
+        <Dimmer active>
+          <Loader>Loading Premium Payment Experience. Please wait...</Loader>
+        </Dimmer>
+      )
 
-  )
+  
+ 
 }
 export default BraintreeDrop
